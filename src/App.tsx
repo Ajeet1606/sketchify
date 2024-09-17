ae70c53fe0ab4f7b91cd4c6b4296ecff9d1ef562
Ajeet Patel <patelajeet1606@gmail.com>
2024-09-17 22:44:33 +0530

    starter code setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold'>Hello</h1>
+     <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
